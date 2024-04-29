a4a71a1cea5b1ac2c0ee0ed895b24f6bdcc6f034
ikharitonov <sarfins2101@gmail.com>
2024-04-29 14:03:35 +0200

    Update chatbot.py

--- chatbot_v1/chatbot.py
+++ chatbot_v1/chatbot.py
@@ -1,7 +1,7 @@
 import openai
 import gradio as gr
 
-openai.api_key = "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"
+openai.api_key = ""
 
 messages = [
     # {"role": "system", "content":
